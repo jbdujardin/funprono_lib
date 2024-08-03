6c8228a92d489da36a307c9ae6b8e41a823824c8
Jean-Baptiste Dujardin <contact@jbdujardin.com>
2024-08-03 17:44:47 +0200

    feat(lib): add current to leagues params

--- lib/src/repositories/leagues_repository.dart
+++ lib/src/repositories/leagues_repository.dart
@@ -4,31 +4,33 @@ class LeaguesRepository {
   LeaguesRepository(this._api);
   final ApiFootballAPI _api;
 
-  Future<List<ApiFootballLeague>> getLeagues(String? season) => _api.getData(
-        path: 'leagues',
-        params: season != null
-            ? {
-                "season": season,
-              }
-            : {},
-        builder: (data) {
-          return (data['response'] as List)
-              .map((json) => ApiFootballLeague.fromJson(json))
-              .toList();
-        },
-      );
+  Future<List<ApiFootballLeague>> getLeagues({
+    String? season,
+    bool? current,
+  }) {
+    final Map<String, String> parameters = {};
+    if (season != null) {
+      parameters["season"] = season;
+    }
+    if (current != null) {
+      parameters["current"] = "true";
+    }
+    return _api.getData(
+      path: 'leagues',
+      params: parameters,
+      builder: (data) {
+        return (data['response'] as List).map((json) => ApiFootballLeague.fromJson(json)).toList();
+      },
+    );
+  }
 
-  Future<ApiFootballLeague> getLeague({required String leagueId}) =>
-      _api.getData(
+  Future<ApiFootballLeague> getLeague({required String leagueId}) => _api.getData(
         path: 'leagues',
         params: {
           "id": leagueId,
         },
         builder: (data) {
-          return (data['response'] as List)
-              .map((json) => ApiFootballLeague.fromJson(json))
-              .toList()
-              .first;
+          return (data['response'] as List).map((json) => ApiFootballLeague.fromJson(json)).toList().first;
         },
       );
 }
