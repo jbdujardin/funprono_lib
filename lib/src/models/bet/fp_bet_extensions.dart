a3d7f8ee083f3de8a4afa39ee90c220b0037685e
Jean-Baptiste Dujardin <contact@jbdujardin.com>
2025-06-01 19:05:39 +0200

    feat(lib): add bet status function

--- lib/src/models/bet/fp_bet_extensions.dart
+++ lib/src/models/bet/fp_bet_extensions.dart
@@ -1,5 +1,11 @@
 import 'package:funprono_lib/funprono_models.dart';
 
+enum BetStatus {
+  loose,
+  good,
+  perfect,
+}
+
 extension BetExtensions on FpBet? {
   bool? get isHomeWin => (this?.homeBet != null && this?.awayBet != null) ? (this!.homeBet! > this!.awayBet!) : null;
 
@@ -38,24 +44,32 @@ extension BetExtensions on FpBet? {
     required FpFixture fixture,
     required bool fixtureNeedToBeFinished,
   }) {
+    final status = this.status(fixture: fixture, fixtureNeedToBeFinished: fixtureNeedToBeFinished);
+    if (status != null) {
+      return status == BetStatus.perfect;
+    }
+    return null;
+  }
+
+  BetStatus? status({
+    required FpFixture fixture,
+    required bool fixtureNeedToBeFinished,
+  }) {
     if (((fixture.type == FixtureType.finished && fixtureNeedToBeFinished) || !fixtureNeedToBeFinished) &&
         this?.isHomeWin != null &&
         this?.isDraw != null &&
         this?.isAwayWin != null &&
         fixture.isHomeWin != null &&
         fixture.isDraw != null &&
-        fixture.isAwayWin != null &&
-        fixture.homePoints != null &&
-        fixture.drawPoints != null &&
-        fixture.awayPoints != null) {
+        fixture.isAwayWin != null) {
       if (isHomeWin! && fixture.isHomeWin!) {
-        return this!.homeBet! == fixture.homeScore! && this!.awayBet! == fixture.awayScore!;
+        return (this!.homeBet! == fixture.homeScore! && this!.awayBet! == fixture.awayScore!) ? BetStatus.perfect : BetStatus.good;
       } else if (isDraw! && fixture.isDraw!) {
-        return this!.homeBet! == fixture.homeScore! && this!.awayBet! == fixture.awayScore!;
+        return (this!.homeBet! == fixture.homeScore! && this!.awayBet! == fixture.awayScore!) ? BetStatus.perfect : BetStatus.good;
       } else if (isAwayWin! && fixture.isAwayWin!) {
-        return this!.homeBet! == fixture.homeScore! && this!.awayBet! == fixture.awayScore!;
+        return (this!.homeBet! == fixture.homeScore! && this!.awayBet! == fixture.awayScore!) ? BetStatus.perfect : BetStatus.good;
       } else {
-        return false;
+        return BetStatus.loose;
       }
     }
     return null;
